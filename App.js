54d22522b2cf5aa5bff3f783cf4ad00173878ec5
lostinnlight <Aditi.K@adypu.edu.in>
2025-09-30 14:15:38 +0530

    Added LoginSignupScreen folder with Login and Signup functionality

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import {Text, View } from 'react-native';
+import LoginScreen from './src/LoginSignupScreen/LoginScreen';
+import SignupScreen from './src/LoginSignupScreen/SignupScreen';
+import SignupNextScreen from './src/LoginSignupScreen/SignupNextScreen';
+import { AuthProvider } from './src/Context/AuthContext';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AuthProvider>
+        <AppNav />
+      </AuthProvider>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
